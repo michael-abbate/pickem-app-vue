5c92c73d64b153d6b44e25825d7d7d39fbe7634b
Michael Abbate <mabbate@mabbate-nyc0121mbp.local>
2022-09-01 01:44:57 -0400

    Add selects for each pick type

--- pickem-app/src/views/SelectedPicks.vue
+++ pickem-app/src/views/SelectedPicks.vue
@@ -1,12 +1,22 @@
 <template>
     <div class = "selected-picks-container">
-        <ul>
-            <li v-for="(pick,index) in picks"
-                :key = "index"
-                :id="pick+'submitted-picks'">
-                {{ pick }}
-            </li>
-        </ul>
+        <table class="select-picks-table">
+            <tr v-for="(type,i) in types_of_picks" :key="i" :class="type+'-row'">
+                <td>
+                    {{ type }}
+                </td>
+                <td>
+                    <select>
+                        <option value="" disabled selected>Select {{ type }}</option>
+                        <option v-for="(pick,index) in picks"
+                            :key = "index"
+                            :id="pick+'submitted-picks'">
+                            {{ pick }}
+                        </option>
+                    </select>
+                </td>
+            </tr>
+        </table>
     </div>
 </template>
 
@@ -14,11 +24,13 @@
 // import { defineComponent } from '@vue/composition-api'
 // import router from '../router'
 
+//TODO: IF SELEECTED FROM PICKS, REMOVE FROM THE LIST!
 export default {
     name: 'SelectedPicks',
     data() {
        return {
-           picks:null
+           picks:null,
+           types_of_picks: ['Favorite','Underdog','Over','Under','LOCK']
        } 
     },
     mounted() {
@@ -26,3 +38,12 @@ export default {
     }
 };
 </script>
+
+<style scoped>
+table {
+    margin: 0 auto;
+    font-size:  1.1vw;
+    border-collapse:separate; 
+    border-spacing:.5em!important;
+}
+</style>
