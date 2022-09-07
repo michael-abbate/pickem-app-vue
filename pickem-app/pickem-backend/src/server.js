ea07c69e0e0aece73ae2be473f114b9cd579e31b
Michael Abbate <mabbate@mabbate-nyc0121mbp.local>
2022-09-07 13:28:04 -0400

    Update backend files to get backend running very simply

--- pickem-app/pickem-backend/src/server.js
+++ pickem-app/pickem-backend/src/server.js
@@ -1,24 +1,28 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import path from 'path';
 import history from 'connect-history-api-fallback';
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 const app = express();
 app.use(bodyParser.json());
 app.use(express.static(__dirname + "../dist"));
-app.use(history());
+// app.use(history());
 
 app.get('/hello', (req, res) => {
-    res.send('Hello! Pickem-app server is running.');
+    res.send('Get hello!!! Pickem-app server is running.');
 });
 
-app.post('/hello', (req, res) => {
-    res.send(`Hello ${req.body.name}`);
-});
 
-app.get('/hello/:name', (req, res) => {
-    res.send(`Hello ${req.params.name}`);
-});
+// app.post('/hello', (req, res) => {
+//     console.log('hello 1');
+//     res.send(`Hello ${req.body.name}`);
+// });
+
+// app.get('/hello/:name', (req, res) => {
+//     console.log('hello 2');
+//     res.send(`Hello ${req.params.name}`);
+// });
 
 // Actual app work... he says to paste in the data to use for the front end?
 // api/products is calling a products array thats hard coded in sample app... in our case we will need to hit exteral API and send back that data.
