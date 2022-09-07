e6f746da2400b566ba3eabf6adda78a8d9612ec4
Michael Abbate <mabbate@mabbate-nyc0121mbp.local>
2022-09-07 18:08:13 -0400

    rework backend to hit API directly in the Vue comp

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+import axios from 'axios';
 const express = require('express');
+
 const port = process.env.PORT || 8080;
 const app = express();
 
@@ -7,6 +9,41 @@ app.use(express.static(__dirname + "/dist/"));
 app.get(/.*/, function(req,res) {
     res.sendFile(__dirname + "/dist/index.html");
 });
+
+// ounted() {
+//     console.log('got to mount.')
+//     axios
+//     .get(all_games_url)
+//     .then(response => {
+//         console.log('SUCCESS');
+//         // Only send next 20 games. No need to send more
+//         console.log(response.data.results.slice(0,20));
+//         this.games = response.data.results.slice(0,20)
+//         })
+//     .catch(error => {
+//         console.log(error)
+//         this.errored = true
+//         })
+// }
+
+// let all_games_url = "https://localhost:8080/api/odds.json?sport=nfl";
+// if (env == "prod") {
+//     all_games_url = "https://areyouwatchingthis.com/api/odds.json?sport=nfl";
+// }
+// app.get("/showpicks", (req, res) => {
+//     axios.get(all_games_url)
+//     .then(response => {
+//         console.log('SUCCESSFULLY HIT API');
+//         // Only send next 20 games. No need to send more
+//         console.log(response.data.results.slice(0,20));
+//         // this.games = response.data.results.slice(0,20)
+//         res.send(response.data.results.slice(0,20))
+//         })
+//     .catch(error => {
+//         console.log(error)
+//         // this.errored = true
+//         })
+// });
 
 // app.listen(port);
 // console.log('Server started...');
--- src/views/ShowPicks.vue
+++ src/views/ShowPicks.vue
@@ -75,7 +75,7 @@ import axios from 'axios';
 let env = process.env.APP_ENV || 'dev';
 console.log(`APP_ENV: ${env}`)
 let all_games_url = "api/odds.json?sport=nfl";
-if (env != "prod") {
+if (env === "prod") {
     all_games_url = "https://areyouwatchingthis.com/api/odds.json?sport=nfl";
 }
 
@@ -98,6 +98,26 @@ export default {
             }
         }
     },
+    async created() {
+    //     const result = await axios.get("https://localhost:8080/api/odds.json?sport=nfl");
+    //     const games = result;
+    //     this.games = games;
+    // },
+    await axios.get(all_games_url)
+    .then(response => {
+        console.log('SUCCESSFULLY HIT API');
+        // Only send next 20 games. No need to send more
+        console.log(response.data.results.slice(0,20));
+        this.games = response.data.results.slice(0,20);
+        // res.send(response.data.results.slice(0,20));
+        })
+    .catch(error => {
+        console.log(error);
+        this.errored = true;
+        })
+    }
+    ,
+
     methods: {    
         selectpicks() {
             // Sends picks to SelectedPicks page where user will then submit picks
@@ -153,22 +173,23 @@ export default {
                 }
             }
         }
-    },
-    mounted() {
-        console.log('got to mount.')
-        axios
-        .get(all_games_url)
-        .then(response => {
-            console.log('SUCCESS');
-            // Only send next 20 games. No need to send more
-            console.log(response.data.results.slice(0,20));
-            this.games = response.data.results.slice(0,20)
-            })
-        .catch(error => {
-            console.log(error)
-            this.errored = true
-            })
     }
+    // ,
+    // mounted() {
+    //     console.log('got to mount.')
+    //     axios
+    //     .get(all_games_url)
+    //     .then(response => {
+    //         console.log('SUCCESS');
+    //         // Only send next 20 games. No need to send more
+    //         console.log(response.data.results.slice(0,20));
+    //         this.games = response.data.results.slice(0,20)
+    //         })
+    //     .catch(error => {
+    //         console.log(error)
+    //         this.errored = true
+    //         })
+    // }
 }
 
 </script>
