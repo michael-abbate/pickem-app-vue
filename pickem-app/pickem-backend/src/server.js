dd1482487091c9c473ac3ec562ff60fad72a3ab3
Michael Abbate <mabbate@mabbate-nyc0121mbp.local>
2022-09-07 11:22:46 -0400

    Make updates to build on heroku

--- pickem-app/pickem-backend/src/server.js
+++ pickem-app/pickem-backend/src/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import history from 'connect-history-api-fallback';
 
 const port = process.env.PORT || 8000
 const app = express();
