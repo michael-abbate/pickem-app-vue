b93ece0b6373e1cfb715714058b34f169a48d6a6
Michael Abbate <mabbate@mabba-NY-1022m.local>
2022-11-20 21:14:39 -0500

    add prod config file with other vars in it

---
+++ pickem-back-end/src/config/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    port: process.env.PORT || 8000,
+    db: {
+        user: "mabbate",
+        host: "localhost",
+        database: "pickem_db",
+        password: "XXX"
+    }
+}
