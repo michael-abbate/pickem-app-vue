dbcd7ab73a827bb6fbe48e766ebb3f6846d29718
Michael Abbate <mabbate@mabba-NY-1022m.local>
2022-11-20 21:15:44 -0500

    intentionally remove config file from git index

--- pickem-back-end/src/config/config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    port: process.env.PORT || 8000,
-    db: {
-        user: "mabbate",
-        host: "localhost",
-        database: "pickem_db",
-        password: "XXX"
-    }
-}
