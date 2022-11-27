420b0fa85cb730dd15580b3464b09bef551944d4
Michael Abbate <mabbate@mabba-NY-1022m.local>
2022-11-27 16:11:06 -0500

    use new fct name for generating access token

--- pickem-back-end/src/controllers/AuthenticationController.js
+++ pickem-back-end/src/controllers/AuthenticationController.js
@@ -125,7 +125,7 @@ module.exports = {
             const userJson = user.toJSON()
             res.send({
                 user: userJson,
-                token: jwtSignUser(userJson)
+                token: generateAccessToken(userJson)
             });
         } catch(err) {
             console.log('catching the error')
