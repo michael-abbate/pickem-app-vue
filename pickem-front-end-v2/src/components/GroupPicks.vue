f3d4c349b4a0d9c3b334f64de5577dc5e5d44a89
Michael Abbate <mabbate@mabba-NY-1022m.local>
2022-11-29 01:10:10 -0500

    style changes to the picks

--- pickem-front-end-v2/src/components/GroupPicks.vue
+++ pickem-front-end-v2/src/components/GroupPicks.vue
@@ -1,37 +1,67 @@
 <template>
     <div id="group-picks">
         <!-- <div v-cloak v-for="team in nflteams" :key = "team.team_id"> -->
-        <table class="picks">
-            <th>
-                NFL Week
-            </th>
-            <th>
-                Username
-            </th>
-            <th>
-                Favorite
-            </th>
-            <th>
-                Underdog
-            </th>
-            <th>
-                Over
-            </th>
-            <th>
-                Under
-            </th>
-            <th>
-                Lock
-            </th>
-            <tr v-for="pick in picks" :key = "pick.username" class="pick-row">
-                <td>{{ pick.nfl_week }}</td>
-                <td>{{ pick.username }}</td>
-                <td>{{ pick.favorite }}</td>
-                <td>{{ pick.underdog }}</td>
-                <td>{{ pick.over }}</td>
-                <td>{{ pick.under }}</td>
-                <td>{{ pick.lock }}</td>
+        <table v-for="pick in picks" :key = "pick.username" class="pick-table">
+            <tr class = "username-row-header">
+                <th colspan = "3">{{ pick.username }}</th>
             </tr>
+            <tr>
+                <td>
+                    &#9711;
+                </td>
+                <td>
+                    Favorite
+                </td>
+                <td>
+                    {{ pick.favorite }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &#9711;
+                </td>
+                <td>
+                    Underdog
+                </td>
+                <td>
+                    {{ pick.underdog }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &#9711;
+                </td>
+                <td>
+                    Over
+                </td>
+                <td>
+                    {{ pick.over }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &#9711;
+                </td>
+                <td>
+                    Under
+                </td>
+                <td>
+                    {{ pick.under }}
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    &#9711;
+                </td>
+                <td>
+                    Lock
+                </td>
+                <td>
+                    {{ pick.lock }}
+                </td>
+            </tr>
+            
+            
         </table>                    
     </div>
 </template>
@@ -59,21 +89,7 @@ export default {
         } catch(error) {
             this.error = error.response.data.error
         }
-    },
-    // methods: {        
-    //     async register() {
-    //         try {       
-    //             const result = await LeaderboardService.leaderboard();
-    //             const users = result.data;
-    //             this.users = users;                                
-
-    //         } catch(error) {
-    //             this.error = error.response.data.error
-
-    //         }
-
-    //     }
-    // }
+    }
 }
 
 </script>
@@ -84,11 +100,25 @@ export default {
     display:flex;
     justify-content:center;
 }
+
+.pick-table {
+    border: 2px solid dodgerblue;
+    margin: 15px;
+    /* border-collapse: collapse; */
+    border-radius:15px;
+    border-spacing: 0;
+}
+
 th {
     padding:10px;
+    color:white;
+    font-size:1.30vw;
+    
 }
 td {
-    padding:10px
+    padding:10px;
+    font-weight: bold;
+    text-align:left;
 }
 
 </style>
