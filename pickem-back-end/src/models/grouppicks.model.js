78e51d340bb03eb39635d486a72eec6389f38974
Michael Abbate <mabbate@mabba-NY-1022m.local>
2022-11-30 00:09:23 -0500

    Remove pick_json field and make all string columns use 1000 char

--- pickem-back-end/src/models/grouppicks.model.js
+++ pickem-back-end/src/models/grouppicks.model.js
@@ -1,27 +1,24 @@
 module.exports = (sequelize, Sequelize) => {
     const GroupPicks = sequelize.define("group_picks", {
       nfl_week: {
-        type: Sequelize.STRING,        
+        type: Sequelize.STRING(1000),        
       },
       username: {
-        type: Sequelize.STRING,        
+        type: Sequelize.STRING(1000),        
       },
       favorite: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000)
       },
       underdog: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000)
       },
       over: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000)
       },
       under: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING(1000)
       },
       lock: {
-        type: Sequelize.STRING
-      },
-      pick_json: {
         type: Sequelize.STRING(1000)
       }
     });
